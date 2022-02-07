e745bfc27c530f9752aad2b5c8a6401455c2d50d
Yusuf Can Celik <yusufcancelik@hotmail.com>
2022-02-07 15:43:14 +0300

    Update error text

--- src/root/index.tsx
+++ src/root/index.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
 
 import '@/public/app.scss';
 import Modal from 'react-modal';
 import App from '@/app/index';
 
-import i18n from '@/i18n';
-
 const rootEl = document.getElementById('root');
 Modal.setAppElement('#modal-root');
 render(
   <>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <App />
   </>,
   rootEl,
 );
--- statics/translations/en/index.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const translation = require('./translation.json');
-
-export default translation;
